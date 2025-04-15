e3d6e7be1749d48028a81fb01232675bef514e92
1amsahil <sahil981156@gmail.com>
2025-04-15 15:08:21 +0530

    ModelAndView Completed

--- src/main/java/com/eternity/springbootMVC/HomeController.java
+++ src/main/java/com/eternity/springbootMVC/HomeController.java
@@ -1,10 +1,10 @@
 package com.eternity.springbootMVC;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -15,12 +15,11 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session)  // HttpServletRequest req
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv)  // HttpServletRequest req, HttpSession session
     {
         int result = num1 + num2;
-
-        session.setAttribute("result",result);
-
-        return "result.jsp";
+        mv.addObject("result",result);
+        mv.setViewName("result.jsp");
+        return mv;
     }
 }
