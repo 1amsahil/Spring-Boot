db154b0e5cabede4ee1ac8b3b8a092fe6fb90b80
1amsahil <sahil981156@gmail.com>
2025-04-15 15:30:39 +0530

    Model Attribute Completed

--- src/main/java/com/eternity/springbootMVC/HomeController.java
+++ src/main/java/com/eternity/springbootMVC/HomeController.java
@@ -1,9 +1,9 @@
 package com.eternity.springbootMVC;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class HomeController {
@@ -14,14 +14,15 @@ public class HomeController {
     }
 
     @RequestMapping("addUser")
-    public ModelAndView addUser(@RequestParam("uid") int uid, @RequestParam("uname") String uname, ModelAndView mv)  // HttpServletRequest req, HttpSession session
+    public String addUser(@ModelAttribute("newuser") User user)  //ModelAttribute is optional when your object name is same
+    // HttpServletRequest req, HttpSession session
     {
-        User user = new User();
-        user.setUid(uid);
-        user.setUname(uname);
+        return "result.jsp";
+    }
 
-        mv.addObject("user",user);
-        mv.setViewName("result.jsp");
-        return mv;
+    @ModelAttribute("course")
+    public String courseName()
+    {
+        return "java";
     }
 }
--- src/main/webapp/result.jsp
+++ src/main/webapp/result.jsp
@@ -3,6 +3,7 @@
     <body>
 
         <h2>User Login Successful</h2>
-        <p>${user}</p>
+        <p>${newuser}</p>
+        <h2>Welcome to ${course} World</h2>
     </body>
 </html>
