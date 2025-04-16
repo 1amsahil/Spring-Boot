a3ffa7e7d36329dc07cf7a8c8a3392a96cc46cd1
1amsahil <sahil981156@gmail.com>
2025-04-16 15:40:14 +0530

    @RestController @GetMapping(@PathVariable) @PostMapping(@RequestBody) @PutMapping @DeleteMapping

---
+++ springboot-rest/src/test/java/com/eternity/springboot_rest/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eternity.springboot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
